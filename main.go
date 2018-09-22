e0d1ea5f232b035ada794d55554519fc34e20823
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-22 18:04:32 +0900

    Return error without terminating in func

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/y-yagi/configure"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
@@ -17,40 +17,55 @@ import (
 	"google.golang.org/api/tasks/v1"
 )
 
-func tokenFile() string {
+func msg(err error) int {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		return 1
+	}
+	return 0
+}
+
+func tokenFile() (string, error) {
 	dir := configure.ConfigDir("gtodo")
 	if err := os.MkdirAll(dir, 0700); err != nil {
-		log.Fatalf("Unable to create config directory: %v", err)
+		return "", errors.Wrap(err, "Unable to open file")
 	}
 
-	return filepath.Join(dir, "token.json")
+	return filepath.Join(dir, "token.json"), nil
 }
 
-func getClient(config *oauth2.Config) *http.Client {
-	tokFile := tokenFile()
+func getClient(config *oauth2.Config) (*http.Client, error) {
+	tokFile, err := tokenFile()
+	if err != nil {
+		return nil, err
+	}
+
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
-		tok = getTokenFromWeb(config)
+		if tok, err = getTokenFromWeb(config); err != nil {
+			return nil, err
+		}
 		saveToken(tokFile, tok)
 	}
-	return config.Client(context.Background(), tok)
+
+	return config.Client(context.Background(), tok), err
 }
 
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
 		"authorization code: \n%v\n", authURL)
 
 	var authCode string
 	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
+		return nil, errors.Wrap(err, "Unable to read authorization code")
 	}
 
 	tok, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
+		return nil, errors.Wrap(err, "Unable to retrieve token from web")
 	}
-	return tok
+	return tok, nil
 }
 
 func tokenFromFile(file string) (*oauth2.Token, error) {
@@ -65,45 +80,53 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	return tok, err
 }
 
-func saveToken(path string, token *oauth2.Token) {
+func saveToken(path string, token *oauth2.Token) error {
 	fmt.Printf("Saving credential file to: %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
+		return errors.Wrap(err, "Unable to cache oauth token")
 	}
 	defer f.Close()
 	json.NewEncoder(f).Encode(token)
+
+	return nil
 }
 
-func main() {
+func run() error {
 	b, err := ioutil.ReadFile(filepath.Join(os.Getenv("HOME"), ".credentials.json"))
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+		return errors.Wrap(err, "Unable to read client secret file")
 	}
 
 	config, err := google.ConfigFromJSON(b, tasks.TasksScope)
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		return errors.Wrap(err, "Unable to parse client secret file to config")
 	}
-	client := getClient(config)
+
+	client, err := getClient(config)
+	if err != nil {
+		return errors.Wrap(err, "Unable to get Client")
+	}
 
 	srv, err := tasks.New(client)
 	if err != nil {
-		log.Fatalf("Unable to retrieve tasks Client %v", err)
+		return errors.Wrap(err, "Unable to retrieve tasks Client")
 	}
 
 	tList, err := srv.Tasklists.List().MaxResults(10).Do()
 	if err != nil {
-		log.Fatalf("Unable to retrieve task lists. %v", err)
+		return errors.Wrap(err, "Unable to retrieve task lists")
 	}
 
 	if len(tList.Items) > 0 {
 		for _, i := range tList.Items {
-			fmt.Printf("## %s\n", i.Title)
 			tasks, err := srv.Tasks.List(i.Id).MaxResults(50).Do()
 			if err != nil {
-				log.Fatalf("Unable to retrieve tasks. %v", err)
+				return errors.Wrap(err, "Unable to retrieve tasks")
 			}
+
+			fmt.Printf("## %s\n", i.Title)
+
 			for _, task := range tasks.Items {
 				due := ""
 				if task.Due != "" {
@@ -117,4 +140,10 @@ func main() {
 	} else {
 		fmt.Print("No task lists found.")
 	}
+
+	return nil
+}
+
+func main() {
+	os.Exit(msg(run()))
 }
