9f02162f2507c28ddbe45337f175190f9d2ec92f
y-yagi <y-yagi@users.noreply.github.com>
2019-08-16 15:36:02 +0900

    Fix lint

--- cache.go
+++ cache.go
@@ -8,15 +8,18 @@ import (
 	"github.com/y-yagi/goext/osext"
 )
 
+// Cache is a type for file cache.
 type Cache struct {
 	path string
 }
 
+// NewCache create a new Cache.
 func NewCache(path string) *Cache {
 	cache := &Cache{path: path}
 	return cache
 }
 
+// Read cache.
 func (c *Cache) Read(key string) ([]byte, error) {
 	file := filepath.Join(c.path, key)
 	if !osext.IsExist(file) {
@@ -25,11 +28,13 @@ func (c *Cache) Read(key string) ([]byte, error) {
 	return ioutil.ReadFile(file)
 }
 
+// Write create a new cache.
 func (c *Cache) Write(key string, value []byte) error {
 	file := filepath.Join(c.path, key)
 	return ioutil.WriteFile(file, value, 0644)
 }
 
+// Delete delete cache.
 func (c *Cache) Delete(key string) error {
 	file := filepath.Join(c.path, key)
 	if !osext.IsExist(file) {
